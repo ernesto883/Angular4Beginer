ec649cd519a8bf380306e77e3f191dde1d157d6e
Szkola <pk1@ckziu.tuchow.pl>
2021-03-08 10:13:24 +0100

    Dodawanie komponentu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
 import { FormsModule } from '@angular/forms';
 import { WellcomeMessageComponent } from './wellcome-message/wellcome-message.component';
+import {LoginComponent} from ' ./login/login.component';
 
 @NgModule({
   declarations: [
